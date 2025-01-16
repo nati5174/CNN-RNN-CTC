352b69f1ac9b6a09deceaac006362fe70a6eba7e
Nethanel Reta <nethanel.n4@gmail.com>
2025-01-16 15:00:01 -0500

    added comments plus Loss fn

--- src/model.py
+++ src/model.py
@@ -15,23 +15,35 @@ class CaptchaModel(nn.Module):
         self.linear1 = nn.Linear(1152, 64)
         self.dp1 = nn.Dropout(0.2) #some neurons will be zeroed out, randmy during training to prevent overfitting
 
-        self.lstm  = nn.LSTM(input_size=64, hidden_size=32, num_layers=2, bidirectional=True, dropout=0.25, batch_first=True)
+        self.lstm  = nn.LSTM(input_size=64, hidden_size=32, num_layers=2, bidirectional=True, dropout=0.25, batch_first=True) # this will output a size of 64 bc bidriectiona;==2. thus 2 hidden states 32+32
 
-        self.linear2 = nn.Linear(64, nums_chars + 1)
+        self.linear2 = nn.Linear(64, nums_chars + 1) 
         
     def forward(self, images, targets=None):
 
-        bs, c, h, w = images.size()
-        x = F.relu(self.convd1(images))
-        x = self.max1(x)
-        x = F.relu(self.convd2(x))
-        x = self.max2(x)
-        x = x.permute(0, 3, 1, 2) #reorder so that we can go from (batcth, filters, hiehgt, width) to (batch, hieght, filters, width)
-        x = x.view(bs, x.size(1),-1) # (0, 3, 1*2)
-        x = self.linear1(x)
-        x = self.dp1(X)
-        x, _ = self.lstm(x)
-        x = self.output(x)
+        bs, c, h, w = images.size() #(5, 3, 75, 300)
+        x = F.relu(self.convd1(images))  #(5, 128, 75, 300)
+        x = self.max1(x)                #(5, 128, 37, 150)
+        x = F.relu(self.convd2(x))  #(5, 64, 37, 150)
+        x = self.max2(x)            #(5, 64, 18, 75)
+        x = x.permute(0, 3, 1, 2)   #(5, 75, 18, 64)                         #reorder so that we can go from (batcth, filters, hiehgt, width) to (batch, hieght, filters, width)
+        x = x.view(bs, x.size(1),-1)  #(5, 75, 1152)  # (0, 3, 1*2)
+        x = self.linear1(x)            #(5, 75, 64)
+        x = self.dp1(X)                #(5, 75, 64)
+        x, _ = self.lstm(x)            # (5, 75, 64)
+        x = self.linear2(x) #output shoulf be (5, 75, 20)
+        x = x.permute(1,0,2) #now (75, 5, 20)
+        if targets is not None:
+                        log_probs = F.log_softmax(x, 2)
+                        input_lengths = torch.full(
+                                size=(bs,), fill_value=log_probs.size(0), dtype=torch.int32
+                                                            ) # vills outa tensor wit array of size bs, with all elements being log_probs.size(0)
+                        output_lengths = torch.ful(size=(bs,), fill_value=targets.size(1), dtype=torch.int32)
+
+                        loss = nn.CTCLoss(blank = 0)(log_probs, targets, input_lengths,output_lengths)
+
+                        return x, loss
+
         return x, None
 
 
@@ -39,8 +51,8 @@ class CaptchaModel(nn.Module):
 
 if __name__ == "__main__":
     cm = CaptchaModel(19)
-    img = torch.rand(1, 3, 75, 300)
-    target = torch.randint(1, 3, 75, 300)
+    img = torch.rand(5, 3, 75, 300) #*bactch, channels-rgb, height, width)
+    target = torch.randint(5, 20, (5, 5))
     x, loss = cm(img, target)
 
          
